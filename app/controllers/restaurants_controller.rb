ad80abf791491865624358f1d45d86d40ad9e9e2
Nicolas Fan <nicfan@gmail.com>
2018-03-21 11:49:41 -0400

    Fixed Carrierwave integration and created Pundit authorizations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,44 @@
 class RestaurantsController < ApplicationController
+
+  def show
+    authorize @restaurant
+  end
+
+  def new
+    authorize @restaurant
+  end
+
+  def create
+    authorize @restaurant
+
+    if @restaurant.save
+      redirect_to root_path
+    else
+      render new_restaurant_path
+    end
+  end
+
+  def edit
+    authorize @restaurant
+  end
+
+  def update
+    authorize @restaurant
+
+    @restaurant.update(restaurant_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    authorize @restaurant
+
+    @restaurant.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:restaurants).permit(:name, :type, :photo)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,13 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
+  validates :user_id, presence: true
+  validates :restaurant_id, presence: true, uniqueness: {scope: :user_id}
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
-  validates :review, presence: true
+  validates :review, presence: true, length: {
+    minimum: 10,
+    maximum: 1000,
+    too_short: "Your review must have at least 10 characters!",
+    too_long: "Your review must be less than 1000 characters!"
+  }
 end
